dc20c94448f1ae7eb21fae41234c89a65c52685b
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-05-06 18:41:11 +0000

    made multiplication by repeated addition form

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Kai Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kai Nguyen
+// Created on: May 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function additionClicked() {
+  const num1 = parseInt(document.getElementById("num1-entered").value)
+  const num2 = parseInt(document.getElementById("num2-entered").value)
+  let counter = 0
+  let answer = 0
+
+  while (counter < num2) {
+    answer += Math.abs(num1)
+    counter++
+  }
+  console.log(answer)
+  document.getElementById("answer").innerHTML = "The answer is = " + answer
+}
+
